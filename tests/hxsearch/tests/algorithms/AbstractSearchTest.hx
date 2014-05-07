362fedd566800584756951db9981382509b40a54
Michel Käser <mk@frontender.ch>
2014-05-07 20:59:43 +0200

    improve test classes quality

--- tests/hxsearch/tests/algorithms/AbstractSearchTest.hx
+++ tests/hxsearch/tests/algorithms/AbstractSearchTest.hx
@@ -6,6 +6,13 @@ package hxsearch.tests.algorithms;
 class AbstractSearchTest extends haxe.unit.TestCase
 {
     /**
+     * Stores the algorithm used to search the Array.
+     *
+     * @var hxsearch.algorithms.TSearchAlgorithm
+     */
+    private var algorithm:hxsearch.algorithms.TSearchAlgorithm;
+
+    /**
      * Stores the Array that is searched.
      *
      * @var Array<Int>
@@ -18,7 +25,8 @@ class AbstractSearchTest extends haxe.unit.TestCase
      */
     override public function setup():Void
     {
-        this.arr = [7, 2, 9, 3, 4, 1, 8, 5, 6];
+        this.algorithm = null;
+        this.arr       = [7, 2, 9, 3, 4, 1, 8, 5, 6];
     }
 
     /**
@@ -27,5 +35,36 @@ class AbstractSearchTest extends haxe.unit.TestCase
     override public function tearDown():Void
     {
         this.arr = null;
+    }
+
+
+    /**
+     * Ensures the search() method doesn't throw an Exception when an empty Array is passed.
+     * This should be ensured by search() being just a wrapper around internalSearch() with
+     * arr.length == 0 check.
+     */
+    public function testWorksWithEmptyArray():Void
+    {
+        try {
+            var found:Bool = this.algorithm.search([], 4, Reflect.compare);
+            assertFalse(found);
+        } catch (ex:Dynamic) {
+            assertFalse(true);
+        }
+    }
+
+    /**
+     * Ensures the search() method works.
+     *
+     * Attn: This test assumes the Reflect.compare method is correctly implemented.
+     * Make sure it works before checking for errors in the algorithm.
+     */
+    public function testSearch():Void
+    {
+        var found:Bool = this.algorithm.search(this.arr, 4, Reflect.compare);
+        assertTrue(found);
+
+        found = this.algorithm.search(this.arr, 10, Reflect.compare);
+        assertFalse(found);
     }
 }
--- tests/hxsearch/tests/algorithms/TestBinarySearch.hx
+++ tests/hxsearch/tests/algorithms/TestBinarySearch.hx
@@ -8,32 +8,11 @@ import hxsearch.tests.algorithms.AbstractSearchTest;
 class TestBinarySearch extends AbstractSearchTest
 {
     /**
-     * Ensures the search() method doesn't throw an Exception when an empty Array is passed.
-     * This should be ensured by search() being just a wrapper around internalSearch() with
-     * arr.length == 0 check.
+     * @{inherit}
      */
-    public function testWorksWithEmptyArray():Void
+    public function new():Void
     {
-        try {
-            var found:Bool = hxsearch.algorithms.BinarySearch.search([], 4, Reflect.compare);
-            assertFalse(found);
-        } catch (ex:Dynamic) {
-            assertFalse(true);
-        }
-    }
-
-    /**
-     * Ensures the search() method works.
-     *
-     * Attn: This test assumes the Reflect.compare method is correctly implemented.
-     * Make sure it works before checking for errors in the algorithm.
-     */
-    public function testSearch():Void
-    {
-        var found:Bool = hxsearch.algorithms.BinarySearch.search(this.arr, 4, Reflect.compare);
-        assertTrue(found);
-
-        found = hxsearch.algorithms.BinarySearch.search(this.arr, 10, Reflect.compare);
-        assertFalse(found);
+        super();
+        this.algorithm = hxsearch.algorithms.BinarySearch;
     }
 }
